48bd7beddbb7904b03cc98c45e8cc3fdc3b9445c
SAM <isam4fx@yandex.ru>
2024-12-14 06:08:48 +0300

    modifications main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import {Provider} from "react-redux"
 import { BrowserRouter } from 'react-router-dom'
 import {store} from "./store.ts";
 
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <StrictMode>
         <BrowserRouter>
